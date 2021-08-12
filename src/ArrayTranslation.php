3c5aeaf8dfda1e467f51aed87cb8bf8c311e2f61
ellisgl <xelliss@gmail.com>
2021-08-12 16:02:39 -0500

    Style fixes. Imports. Removed two types that were not getting translated and will be / are removed in future PHP releases, or just wasn't possible to do. Removed dead code.

--- src/ArrayTranslation.php
+++ src/ArrayTranslation.php
@@ -2,72 +2,57 @@
 
 namespace GeekLab;
 
+use \Exception;
 use GeekLab\ArrayTranslation\TranslationInterface;
 
 /**
  * Class ArrayTranslation - Factory
+ *
  * @package geeklab\arraytranslation
  */
 class ArrayTranslation
 {
     public static function create(string $type = '', string $handler = '')
     {
-        if(!empty($handler))
-        {
-            // Make sure handler is accessible
-            if(!class_exists($handler))
-            {
-                throw new \Exception('Class "' . $handler . '" does not exist or is not accessible.');
+        if (!empty($handler)) {
+            // Make sure handler is accessible.
+            if (!class_exists($handler)) {
+                throw new Exception("Class '$handler' does not exist or is not accessible.");
             }
 
             $translator = new $handler();
 
-            if(!$translator instanceof TranslationInterface)
-            {
-                throw new \Exception('Class "' . $handler . '" is not an instance of GeekLab\ArrayTranslation\TranslationInterface.');
+            if (!$translator instanceof TranslationInterface) {
+                throw new Exception(
+                    "Class '$handler' is not an instance of GeekLab\ArrayTranslation\TranslationInterface."
+                );
             }
 
             return $translator;
         }
 
-        switch (strtolower($type))
-        {
+        switch (strtolower($type)) {
             case 'php_serialize':
                 return new ArrayTranslation\PHPSerialize();
-                break;
 
             case 'php_binary':
                 return new ArrayTranslation\PHPBinary();
-                break;
 
             case 'php':
                 return new ArrayTranslation\PHP();
-                break;
-
 
             case 'json':
                 return new ArrayTranslation\JSON();
-                break;
 
             case 'xml':
                 return new ArrayTranslation\XML();
-                break;
 
             case 'yaml':
                 return new ArrayTranslation\YAML();
-                break;
-
-            case 'wddx':
-                //return new ArrayTranslation\WDDX();
-                //break;
-
-            case 'igbinary':
-                //return new ArrayTranslation\igbinary();
-                //break;
 
             default:
                 // Throw invalid 'data' error
-                throw new \Exception('Invalid data type: "' . $type . '" is not supported.');
+                throw new Exception("Invalid data type: '$type' is not supported.");
         }
     }
 }
